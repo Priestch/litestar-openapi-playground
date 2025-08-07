7ba42da7c65defeb0b4c378aecce27cd59834f25
Priestch <priestch@gmail.com>
2025-08-07 22:27:48 +0800

    enable scalar openapi

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from litestar import Litestar, get
+from litestar.openapi import OpenAPIConfig
+from litestar.openapi.plugins import ScalarRenderPlugin
 
 
 @get("/")
 async def hello_world() -> dict[str, str]:
-    """Handler function that returns a greeting dictionary."""
     return {"hello": "world"}
 
 
-app = Litestar(route_handlers=[hello_world])
+openapi_config = OpenAPIConfig(title="My API", version="1.0.0", render_plugins=[ScalarRenderPlugin()])
+
+app = Litestar(route_handlers=[hello_world],
+               openapi_config=openapi_config)
